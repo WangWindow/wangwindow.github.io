1e225c69caf6c1bda801aa4db95a95c64ac0186d
WangWindow <1598593280@qq.com>
2026-08-07 23:05:12 +0800

    feat(search): integrate astro-pagefind

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import tailwind from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
+import pagefind from "astro-pagefind";
 import robotsTxt from "astro-robots-txt";
 import webmanifest from "astro-webmanifest";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
@@ -24,6 +25,7 @@ export default defineConfig({
   integrations: [
     expressiveCode(expressiveCodeOptions),
     icon(),
+    pagefind(),
     sitemap(),
     mdx(),
     robotsTxt(),
